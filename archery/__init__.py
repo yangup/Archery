a54f478374bd078e2ca0afac3eacc3eca3cbf75c
yangpu <yangpu@ksmglobal.tech>
2024-03-15 11:17:35 +0800

    bug

--- archery/__init__.py
+++ archery/__init__.py
@@ -1,2 +1,3 @@
-version = (1, 10, 0)
-display_version = ".".join(str(i) for i in version)
+# version = (1, 10, 0, '__1')
+# display_version = ".".join(str(i) for i in version)
+display_version = "1.10.0__3"
