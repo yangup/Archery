da888525e4efc47f9fe8c3f432b04a847dcb026b
yangpu <yangpu@ksmglobal.tech>
2024-04-22 15:30:55 +0800

    bug and log

--- sql/engines/mysql.py
+++ sql/engines/mysql.py
@@ -3,7 +3,6 @@ import logging
 import traceback
 import MySQLdb
 import pymysql
-import json
 import re
 
 import schemaobject
@@ -57,10 +56,6 @@ class MysqlEngine(EngineBase):
     test_query = "SELECT 1"
 
     def __init__(self, instance=None):
-
-        logger.warning("MysqlEngine.__init__")
-        logger.warning(json.dump(instance))
-
         super().__init__(instance=instance)
         self.config = SysConfig()
         self.inc_engine = GoInceptionEngine()
